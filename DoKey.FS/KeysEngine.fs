3cb9eaf947dc62fa8ac68c63fe02aebab8d68cdc
Dominik Torhan <dominik.torhan@gmail.com>
2019-07-22 15:52:24 +0200

    xx

---
+++ DoKey.FS/KeysEngine.fs
@@ -0,0 +1,6 @@
+﻿module KeysEngine
+
+open DoKey.FS
+
+let Perform(appState:AppState, inputKey: InputKey) =
+    appState
--- DoKey.Tests/TestKeysEngine.fs
+++ DoKey.Tests/TestKeysEngine.fs
@@ -3,14 +3,26 @@
 open Xunit 
 
 open DoKey.FS
-open DoKey
-open TrayApp2
-
+open TrayApp2  
+ 
 [<Theory>]  
 [<InlineData("", "")>]
-let Test(expected:string, key:string)=    
-    let x = new cKeysEngine();
-    let eventData = new KeyEventData("", KeyEventType.Down)
-    //let actual = InputKeyToStr(new InputKey(key))
-    //Assert.Equal (expected, actual) 
+let TestStateNormal(expected:string, key:string) =     
+    let appState = AppStatesFactory.AppStateNormal  
+    let configuration = new Configuration()
+    let eventData = new KeyEventData(key, KeyEventType.Down)
+    let keysEngine = new cKeysEngine()
+    keysEngine.Configuration <- configuration 
+    keysEngine.AppState <- appState 
+    keysEngine.KeyEventData <- eventData 
+    let output = keysEngine.ProcessKey
     Assert.True(true)
+
+//[<Theory>]  
+//[<InlineData("", "")>]
+//let Test(expected:string, key:string)=    
+//    let x = new cKeysEngine();
+//    let eventData = new KeyEventData("", KeyEventType.Down)
+//    //let actual = InputKeyToStr(new InputKey(key))
+//    //Assert.Equal (expected, actual) 
+//    Assert.True(true)
