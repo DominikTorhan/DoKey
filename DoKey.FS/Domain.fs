5dfe0b6eb1e87b9b906bb1b377bd14b8e615662b
dominik <dominik.torhan@gmail.com>
2019-09-10 19:50:48 +0200

    add domain

---
+++ DoKey.FS/Domain.fs
@@ -0,0 +1,16 @@
+﻿namespace DoKey.FS 
+ 
+
+module Domain =   
+  
+    type State = Off = 0 | Normal = 1 | Insert = 2
+
+    type SendKey =
+        { trigger: string
+          send: string 
+          isCaps: bool }
+
+ 
+    type KeysList(keyList:list<SendKey>) = 
+        member this.KeyList = keyList
+
