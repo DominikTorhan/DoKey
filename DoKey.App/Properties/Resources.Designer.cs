488285562c466ed42a16d107e4927ce16f83358a
dominik <dominik.torhan@gmail.com>
2019-09-20 19:57:56 +0200

    folders names changed

--- DoKey.App/Properties/Resources.Designer.cs
+++ DoKey.App/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace DoKey.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TrayApp2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DoKey.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
